beaadc86e298c8b3fde7d3670990f9dec2a31cf4
dogiloki <wolftri2002@gmail.com>
2023-04-25 02:43:25 -0600

    Se agrego canales para registrar logs, error e info

--- config/logging.php
+++ config/logging.php
@@ -126,6 +126,21 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        // Custom
+        
+        'info' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/info.log'),
+            'level' => 'info',
+        ],
+        
+        'error' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/error.log'),
+            'level' => 'error',
+        ]
+
     ],
 
 ];
